c76d1c80e3887deeeda448d7c62f4fb73199fbf1
Sander van Driel <sander.vandriel@gmail.com>
2026-09-04 16:04:56 +0200

    neumorphic

--- src/components/Toggle.vue
+++ src/components/Toggle.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="toggle-container inline-flex px-1 py-1 bg-slate-400 dark:bg-slate-800 rounded-md gap-1"
+    class="toggle-container inline-flex px-1 py-1 rounded-md gap-1 shadow-in-xs dark:shadow-in-xs-dark"
   >
     <div v-for="value in values" :key="value.id">
       <button
         @click="onClicked(value.id)"
         :aria-pressed="isPressed(value.id)"
-        class="inline-flex px-2 py-1.5 rounded-md font-mono tracking-widest text-slate-700 dark:text-slate-500 aria-pressed:text-slate-100 dark:aria-pressed:text-slate-100 cursor-pointer uppercase text-2xs"
+        class="inline-flex px-2 py-1.5 rounded-md font-mono tracking-widest text-primary-400 dark:text-primary-500 aria-pressed:text-primary-900 dark:aria-pressed:text-primary-100 cursor-pointer uppercase text-2xs"
       >
         {{ value.label }}
       </button>
@@ -31,8 +31,6 @@ function onClicked(id: string) {
 
 <style lang="css" scoped>
 button {
-  /* font-size: 10px; */
-
   transition:
     transform 0.06s,
     box-shadow 0.06s,
@@ -42,8 +40,6 @@ button:active {
   transform: translateY(2px);
 }
 button[aria-pressed="true"] {
-  /* color: #fff; */
-  background: linear-gradient(180deg, var(--rubber-hi), var(--rubber));
   box-shadow: inset 0 1px 2px rgba(0, 0, 0, 0.5);
   transform: none;
 }
