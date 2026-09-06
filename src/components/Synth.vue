a7c425f9d332d9eecc160e252401a3454d15316a
Sander van Driel <sander.vandriel@gmail.com>
2026-09-06 13:02:31 +0200

    prettier

--- src/components/Synth.vue
+++ src/components/Synth.vue
@@ -2,24 +2,29 @@
   <div
     class="synth bg-primary-300 dark:bg-primary-700 min-h-100 p-3 rounded-2xl b-3 shadow-out dark:shadow-out-dark"
   >
-    synth
-    <div class="">
+    <div class="flex flex-col">
       <Toggle
         :values="[
           { id: 'sine', label: 'Sin' },
           { id: 'square', label: 'Square' },
-          { id: 'triangle', label: 'Triangle' },
+          { id: 'triangle', label: 'Triangle' }
         ]"
         v-model="waveForm"
       />
+
+      <div class="flex flex-row gap-5 items-center ml-3">
+        <SynthButton>osc 1</SynthButton>
+        <SynthButton>osc 2</SynthButton>
+      </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from "vue";
-import Toggle from "./Toggle.vue";
+import { ref } from 'vue'
+import Toggle from './Toggle.vue'
+import SynthButton from './SynthButton.vue'
 
-const waveForm = ref("sine");
+const waveForm = ref('sine')
 </script>
 
 <style lang="css" scoped>
---
+++ src/components/SynthButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button
+    class="osc-button flex flex-row items-center justify-center m-5 p-3 rounded-sm"
+    type="button"
+    aria-pressed="false"
+    aria-label="Oscillator 1"
+  >
+    <!-- <span class="led" aria-hidden="true"></span>
+
+    <span class="wave-wrap">
+      <svg class="wave" viewBox="0 0 130 80" aria-hidden="true">
+        <path class="line" d="M5,55 C25,15 40,15 65,40 C90,65 105,65 125,25" />
+      </svg>
+    </span> -->
+
+    <span class="label"><slot /></span>
+    <!-- <span class="underline"></span> -->
+  </button>
+</template>
+<style scoped>
+button {
+  height: 5rem;
+  width: 5rem;
+  /* background-color: var(--color-red-300); */
+  box-shadow: var(--shadow-out);
+}
+</style>
