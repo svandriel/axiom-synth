29e538fad4f049eea42d5ec20208e92f82d783c8
Sander van Driel <sander.vandriel@gmail.com>
2026-09-07 11:27:29 +0200

    multi osc

--- src/components/Synth.vue
+++ src/components/Synth.vue
@@ -1,27 +1,14 @@
 <template>
   <div
-    class="synth b-3 min-h-100 rounded-2xl bg-default p-3 shadow-out dark:bg-default-dark dark:shadow-out-dark"
+    class="synth b-3 min-h-100 min-w-5xl rounded-2xl bg-default p-3 shadow-out dark:bg-default-dark dark:shadow-out-dark"
   >
-    <div class="grid grid-cols-12 grid-rows-6 gap-3">
-      <div class="col-span-4 row-start-1 section">
-        Osc 1
-
-        <Knob v-model="osc1Gain" :min="0" :max="100" :default="50" />
-      </div>
-      <div class="col-span-4 row-start-2 section">Osc 2</div>
-      <div class="col-span-4 row-start-3 section">Osc 3</div>
+    <div class="grid grid-cols-12 grid-rows-3 gap-3">
+      <OscillatorPanel class="col-span-4 row-start-1" v-model="osc1" />
+      <OscillatorPanel class="col-span-4 row-start-2" v-model="osc2" />
+      <OscillatorPanel class="col-span-4 row-start-3" v-model="osc3" />
     </div>
 
     <div class="flex flex-col">
-      <Toggle
-        :values="[
-          { id: 'sine', label: 'Sin' },
-          { id: 'square', label: 'Square' },
-          { id: 'triangle', label: 'Triangle' },
-        ]"
-        v-model="waveForm"
-      />
-
       <div class="ml-3 flex flex-row items-center gap-5">
         <SynthButton>osc 1</SynthButton>
         <SynthButton>osc 2</SynthButton>
@@ -30,22 +17,27 @@
   </div>
 </template>
 <script setup lang="ts">
-import { ref, watch } from 'vue';
-import Toggle from './Toggle.vue';
+import { reactive } from 'vue';
+import type { OscillatorConfig } from '../types/oscillator-config.ts';
+import OscillatorPanel from './OscillatorPanel.vue';
 import SynthButton from './SynthButton.vue';
-import Knob from './Knob.vue';
 
-const waveForm = ref('sine');
-
-const osc1Gain = ref(0.5);
-
-watch(osc1Gain, newValue => {
-  console.log('osc1Gain changed:', newValue);
+const osc1 = reactive<OscillatorConfig>({
+  detune: -0.1,
+  pitch: 0,
+  gain: 75,
+  waveform: 'saw',
+});
+const osc2 = reactive<OscillatorConfig>({
+  detune: 0.1,
+  pitch: 0,
+  gain: 75,
+  waveform: 'saw',
+});
+const osc3 = reactive<OscillatorConfig>({
+  detune: 0,
+  pitch: -12,
+  gain: 42.6,
+  waveform: 'sine',
 });
 </script>
-
-<style lang="css" scoped>
-.synth {
-  /* box-shadow: -3px -1px 19px 14px var(--color-primary-300/50); */
-}
-</style>
