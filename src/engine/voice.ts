a33e100ea8aaa583026f6d5da17ee3dfae614760
Sander van Driel <sander.van.driel@rabobank.nl>
2026-09-09 15:57:13 +0200

    extract envelope

--- src/engine/voice.ts
+++ src/engine/voice.ts
@@ -1,10 +1,13 @@
 import type { EnvelopeConfig } from '../types/envelope-config';
+import { Envelope } from './envelope';
 
 export abstract class Voice {
   protected readonly ctxt: AudioContext;
   protected readonly audioSink: AudioNode;
 
-  protected readonly ampEnv: GainNode;
+  private readonly chokeTime = 0.003;
+
+  protected readonly ampEnvelope: Envelope;
   // private readonly filterEnv: GainNode;
   private oscillatorsActive = false;
   private cleanupTimer: ReturnType<typeof setTimeout> | null = null;
@@ -13,16 +16,11 @@ export abstract class Voice {
   public endTime = 0;
   public lastUsed = 0;
 
-  private readonly maxVolume = 0.2;
-
   constructor(ctxt: AudioContext, audioSink: AudioNode) {
     this.ctxt = ctxt;
     this.audioSink = audioSink;
 
-    this.ampEnv = this.ctxt.createGain();
-    this.ampEnv.gain.setValueAtTime(0, this.ctxt.currentTime);
-
-    this.ampEnv.connect(this.audioSink);
+    this.ampEnvelope = new Envelope(ctxt, audioSink);
   }
 
   protected abstract createOscillators(now: number): void;
@@ -39,16 +37,11 @@ export abstract class Voice {
   }
 
   /**
-   * Executes a 3ms micro-fade parameter envelope to truncate a stolen note cleanly
+   * Executes a micro-fade parameter envelope to truncate a stolen note cleanly
    */
-  public fastChoke(time: number): void {
-    this.ampEnv.gain.cancelScheduledValues(time);
-    this.ampEnv.gain.setValueAtTime(this.ampEnv.gain.value, time);
-    // Explicit micro-ramp down to prevent audio artifacts/clicks
-    this.ampEnv.gain.linearRampToValueAtTime(0, time + 0.003);
-
-    // this.filter.frequency.cancelScheduledValues(time);
-    this.endTime = time + 0.003;
+  fastChoke(time: number): void {
+    this.ampEnvelope.fastChoke(this.chokeTime, time);
+    this.endTime = time + this.chokeTime;
   }
 
   noteOn(
@@ -74,27 +67,9 @@ export abstract class Voice {
     }
 
     this.createOscillators(now);
-
-    const targetVolume = (velocity / 127) * this.maxVolume;
-
-    this.ampEnv.gain.cancelScheduledValues(now);
-
     this.setOscillatorNote(noteNumber, now);
 
-    this.ampEnv.gain.setValueAtTime(this.ampEnv.gain.value, now);
-    this.ampEnv.gain.setTargetAtTime(
-      targetVolume,
-      now,
-      ampEnvelope.attackSeconds / 3,
-    );
-
-    const decayStartTime = now + ampEnvelope.attackSeconds;
-    const sustainVolume = targetVolume * ampEnvelope.sustainLevel;
-    this.ampEnv.gain.setTargetAtTime(
-      sustainVolume,
-      decayStartTime,
-      ampEnvelope.decaySeconds / 3,
-    );
+    this.ampEnvelope.noteOn(velocity, ampEnvelope, now);
 
     this.oscillatorsActive = true;
     this.endTime = Infinity;
@@ -109,9 +84,7 @@ export abstract class Voice {
       this.cleanupTimer = null;
     }
 
-    this.ampEnv.gain.cancelScheduledValues(now);
-    this.ampEnv.gain.setValueAtTime(this.ampEnv.gain.value, now);
-    this.ampEnv.gain.setTargetAtTime(0, now, ampEnvelope.releaseSeconds / 3);
+    this.ampEnvelope.noteOff(ampEnvelope, now);
 
     // 5 time-constants completely flattens setTargetAtTime
     this.endTime = now + ampEnvelope.releaseSeconds * 5;
@@ -137,7 +110,7 @@ export abstract class Voice {
       this.destroyOscillators();
       this.oscillatorsActive = false;
     }
-    this.ampEnv.disconnect();
+    this.ampEnvelope.disconnect();
     // this.filterEnv.disconnect();
     // this.filter.disconnect();
   }
