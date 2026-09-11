bda3d0edf241b052edecf44886398a3d285c7168
Sander van Driel <sander.vandriel@gmail.com>
2026-09-11 15:58:50 +0200

    feat: better knob control

--- src/utils/time-display.ts
+++ src/utils/time-display.ts
@@ -3,6 +3,10 @@ export function timeDisplay(seconds: number) {
     return `${(seconds * 1000).toFixed(2)} ms`;
   } else if (seconds < 1) {
     return `${Math.round(seconds * 1000)} ms`;
+  } else {
+    return `${seconds.toFixed(1)} s`;
   }
-  return `${seconds.toFixed(2)} s`;
+}
+export function timeDisplayMs(millis: number) {
+  return timeDisplay(0.001 * millis);
 }
