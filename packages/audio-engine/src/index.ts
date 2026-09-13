6148ca4540272579726f85e4804441a88f1a5719
Sander van Driel <sander.vandriel@gmail.com>
2026-09-13 16:41:00 +0200

    refactor: move audio engine into @axiom/audio-engine package

--- packages/audio-engine/src/index.ts
+++ packages/audio-engine/src/index.ts
@@ -1,1 +1,7 @@
-export {};
+export * from './engine';
+export type * from './types/envelope-config';
+export type * from './types/filter-config';
+export type * from './types/oscillator-config';
+export type * from './types/fixed-array';
+export type * from './types/waveshaper-config';
+export type { WaveshaperType } from './engine/waveshaper';
