345bcd55d0c3216b93d4aba7a9e6957d1a81449f
Sander van Driel <sander.vandriel@gmail.com>
2026-09-16 16:12:51 +0200

    feat(engine): add LFO types and constants

--- packages/audio-engine/src/index.ts
+++ packages/audio-engine/src/index.ts
@@ -4,5 +4,6 @@ export type * from './types/filter-config';
 export type * from './types/oscillator-config';
 export type * from './types/fixed-array';
 export type * from './types/waveshaper-config';
+export type * from './types/lfo-config';
 export type { WaveshaperType } from './engine/waveshaper';
 export type { FilterType } from './engine/filter';
